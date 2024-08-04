1679a0690f3fd7fa61d385b273866bfe4daf2d5e
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-08-04 12:53:52 +0530

    Refactor variable name in db connection code

--- src/db/index.js
+++ src/db/index.js
@@ -5,8 +5,8 @@ import {DB_NAME} from '../constants.js'
 const connectDB = async ()=>{
 
     try {
-        const coonnectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST : ${coonnectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST : ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error(`MONGODB connection FAILED ${error}`)
         process.exit(1)
